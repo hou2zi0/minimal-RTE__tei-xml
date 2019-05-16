95e3d469018b1aa7aaff6d2d725e3cdbf9882f7b
hou2zi0 <max.gruentgens@adwmainz.de>
2019-05-16 21:55:24 +0200

    Added first bulk of main functionality and info files.

--- JS/minimal_quill_tei.js
+++ JS/minimal_quill_tei.js
@@ -1,58 +1,56 @@
 let Inline = Quill.import('blots/inline');
 
-let source;
+let source = [{
+  "text": "Example text."
+}];
 
-const readFile = function () {
-	d3.json(CONFIG.reader.result, function (data) {
-			return data;
-		})
-		.then(function (data) {
-			source = data;
-			quill.setText(source[0].text);
-			return data;
-		}, function (error) {
-			console.log(error);
-			CONFIG.fileLoaded = false;
-		});
+const readFile = function() {
+  d3.json(CONFIG.reader.result, function(data) {
+      return data;
+    })
+    .then(function(data) {
+      source = data;
+      quill.setText(source[0].text);
+      return data;
+    }, function(error) {
+      console.log(error);
+      CONFIG.fileLoaded = false;
+    });
 };
 
 const CONFIG = {
-	"fileLoaded": false
+  "fileLoaded": false
 };
 
 const Up = document.getElementById('poly-file')
-	.addEventListener("change", (e) => {
+  .addEventListener("change", (e) => {
 
-		const filehandle = document.getElementById('poly-file')
-			.files[0];
-		CONFIG.reader = new FileReader();
+    const filehandle = document.getElementById('poly-file')
+      .files[0];
+    CONFIG.reader = new FileReader();
 
-		if (filehandle) {
-			CONFIG.reader.readAsDataURL(filehandle);
-		}
+    if (filehandle) {
+      CONFIG.reader.readAsDataURL(filehandle);
+    }
 
-		CONFIG.reader.addEventListener("load", () => {
-			readFile();
-		}, false);
-	});
+    CONFIG.reader.addEventListener("load", () => {
+      readFile();
+    }, false);
+  });
 
 
 class SPAN extends Inline {
-	static create(value) {
-		let node = super.create();
-		// Sanitize url value if desired
-		node.setAttribute('data-type', value[0]);
-		return node;
-	}
-	static formats(node) {
-		// We will only be called with a node already
-		// determined to be a Link blot, so we do
-		// not need to check ourselves
-		node = {
-			'annotation': node.getAttribute('data-type'),
-		};
-		return node;
-	}
+  static create(value) {
+    let node = super.create();
+    node.setAttribute('data-type', value[0]);
+    return node;
+  }
+  static formats(node) {
+    node = {
+      'annotation': node.getAttribute('data-type'),
+    };
+    return node;
+  }
 }
 
 // Registering
@@ -61,70 +59,70 @@ SPAN.tagName = 'span';
 Quill.register(SPAN);
 
 var quill = new Quill('#editor-container', {
-	modules: {
-		toolbar: true
-	},
-	theme: 'snow'
+  modules: {
+    toolbar: true
+  },
+  theme: 'snow'
 });
 
 
 
 ['person', 'place', 'org'].forEach((item) => {
-	document.getElementById(`${item.toLowerCase()}-button`)
-		.addEventListener('click', function () {
-			if (quill.getFormat()
-				.SPAN === undefined) {
-				console.log('undef');
-				let {
-					index,
-					length
-				} = quill.getSelection();
-				quill.format('SPAN', [item]);
-			} else if (quill.getFormat()
-				.SPAN.annotation === item) {
-				let {
-					index,
-					length
-				} = quill.getSelection();
-				quill.removeFormat(index, length);
-			} else {
-				let {
-					index,
-					length
-				} = quill.getSelection();
-				quill.format('SPAN', [item]);
-			}
-		});
+  document.getElementById(`${item.toLowerCase()}-button`)
+    .addEventListener('click', function() {
+      if (quill.getFormat()
+        .SPAN === undefined) {
+        console.log('undef');
+        let {
+          index,
+          length
+        } = quill.getSelection();
+        quill.format('SPAN', [item]);
+      } else if (quill.getFormat()
+        .SPAN.annotation === item) {
+        let {
+          index,
+          length
+        } = quill.getSelection();
+        quill.removeFormat(index, length);
+      } else {
+        let {
+          index,
+          length
+        } = quill.getSelection();
+        quill.format('SPAN', [item]);
+      }
+    });
 });
 
 
 // Download
-const prepareDownload = function (data, filename) {
-	const content = data;
-	const element = document.createElement('a');
-	element.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(content));
-	element.setAttribute('download', filename);
-	element.style.display = 'none';
-	document.body.appendChild(element);
-	element.click();
-	document.body.removeChild(element);
+const prepareDownload = function(data, filename) {
+  const content = data;
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(content));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 };
 
-const formatTEI = function (editor) {
-	let re = /<span data-type="(.*?)">(.*?)<\/span>/g
-	editor = editor.replace(re, `<rs type="$1">$2</rs>`);
-	return editor;
+const formatTEI = function(editor) {
+  let re = /<span data-type="(.*?)">(.*?)<\/span>/g
+  editor = editor.replace(re, `<rs type="$1">$2</rs>`);
+  return editor;
 };
 
 document.getElementById('download-button')
-	.addEventListener('click', function () {
-		console.log(quill.getContents());
-		let editor = document.getElementById('editor-container')
-			.getElementsByClassName('ql-editor')[0].innerHTML;
-		console.log(`<doc>${editor}</doc>`);
-		console.log(quill.getText());
-		console.log(quill.getContents());
-		const out = `<TEI>
+  .addEventListener('click', function() {
+    console.log(quill.getContents());
+    let editor = document.getElementById('editor-container')
+      .getElementsByClassName('ql-editor')[0].innerHTML;
+    console.log(`<doc>${editor}</doc>`);
+    console.log(quill.getText());
+    console.log(quill.getContents());
+    const out = `<TEI xmlns="http://www.tei-c.org/ns/1.0">
       <teiHeader>
             <fileDesc>
                 <titleStmt>
@@ -152,74 +150,74 @@ document.getElementById('download-button')
         </text>
     </TEI>`;
 
-		prepareDownload(out, "bing.xml");
-	});
+    prepareDownload(out, "bing.xml");
+  });
 
 // Prev next buttons
 let current = 0;
 
 document.getElementById('pre-button')
-	.addEventListener('click', function () {
-		current -= 1;
-		if (current >= 0) {
-			console.log(current);
-			console.log(source.length);
-			source[current + 1].delta = document.getElementById('editor-container')
-				.getElementsByClassName('ql-editor')[0].innerHTML;
-			console.log(source[current + 1].delta);
-			if (source[current].delta) {
-				document.getElementById('editor-container')
-					.getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
-			} else {
-				quill.setText(source[current].text);
-			}
-		} else {
-			console.log('end of array');
-			current = 0;
-		}
-	});
+  .addEventListener('click', function() {
+    current -= 1;
+    if (current >= 0) {
+      console.log(current);
+      console.log(source.length);
+      source[current + 1].delta = document.getElementById('editor-container')
+        .getElementsByClassName('ql-editor')[0].innerHTML;
+      console.log(source[current + 1].delta);
+      if (source[current].delta) {
+        document.getElementById('editor-container')
+          .getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
+      } else {
+        quill.setText(source[current].text);
+      }
+    } else {
+      console.log('end of array');
+      current = 0;
+    }
+  });
 
 document.getElementById('next-button')
-	.addEventListener('click', function () {
-		current += 1;
-		if (current < source.length) {
-			console.log(current);
-			console.log(source.length);
-			source[current - 1].delta = document.getElementById('editor-container')
-				.getElementsByClassName('ql-editor')[0].innerHTML;
-			console.log(source[current - 1].delta);
-			if (source[current].delta) {
-				document.getElementById('editor-container')
-					.getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
-			} else {
-				quill.setText(source[current].text);
-			}
-		} else {
-			console.log('end of array');
-			current = source.length - 1;
-		}
-	});
+  .addEventListener('click', function() {
+    current += 1;
+    if (current < source.length) {
+      console.log(current);
+      console.log(source.length);
+      source[current - 1].delta = document.getElementById('editor-container')
+        .getElementsByClassName('ql-editor')[0].innerHTML;
+      console.log(source[current - 1].delta);
+      if (source[current].delta) {
+        document.getElementById('editor-container')
+          .getElementsByClassName('ql-editor')[0].innerHTML = source[current].delta;
+      } else {
+        quill.setText(source[current].text);
+      }
+    } else {
+      console.log('end of array');
+      current = source.length - 1;
+    }
+  });
 
 document.getElementById('editor-container')
-	.addEventListener('keydown', function (event) {
-		event.stopPropagation();
-	});
+  .addEventListener('keydown', function(event) {
+    event.stopPropagation();
+  });
 
 document.body
-	.addEventListener('keydown', function (event) {
-		// Add arrow key navigation through sources
-		switch (event.keyCode) {
-		case 37:
-			console.log('Left key pressed');
-			break;
-		case 38:
-			console.log('Up key pressed');
-			break;
-		case 39:
-			console.log('Right key pressed');
-			break;
-		case 40:
-			console.log('Down key pressed');
-			break;
-		}
-	});
+  .addEventListener('keydown', function(event) {
+    // Add arrow key navigation through sources
+    switch (event.keyCode) {
+      case 37:
+        console.log('Left key pressed');
+        break;
+      case 38:
+        console.log('Up key pressed');
+        break;
+      case 39:
+        console.log('Right key pressed');
+        break;
+      case 40:
+        console.log('Down key pressed');
+        break;
+    }
+  });
